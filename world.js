d114b8df780e52612a51df4010d800aae2c50267
Tithi Chheda <tithichheda30@email.com>
2025-06-05 10:34:03 +0530

    i learned function

---
+++ world.js
@@ -0,0 +1,13 @@
+// const prompt = require('prompt-sync')();
+// const name = prompt("what is your name? ");
+// console.log("hello,"+ name + "!" );
+
+
+
+
+function sq(x){
+    let ans = 0;
+    ans = x*x;
+    return ans;
+}
+console.log("the answer is:" + sq(4));
