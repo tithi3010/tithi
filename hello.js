a0da22646f35357092ca40b8d3c9fc4063182a82
Tithi Chheda <tithichheda30@email.com>
2025-06-05 09:12:54 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+let val=19;
+val='hello';
+val=falseval='world';
+console.log(val)
+
+const pi=3.14
+console.log(pi)
+
+let fil=7
+if(fil%2==0)
+{
+    console.log("number is even")
+}else{
+    console.log("number is odd")
+}
+
+
+for(let i=0;i<5;i++){
+    console.log(i)
+}
